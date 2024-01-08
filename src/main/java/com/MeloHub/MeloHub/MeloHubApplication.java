785dd5f9dbfa6cbf5cd056818cd28351d579b660
Dmitrii <rydima0469@gmail.com>
2024-01-08 14:35:20 +0300

    first add

---
+++ src/main/java/com/MeloHub/MeloHub/MeloHubApplication.java
@@ -0,0 +1,13 @@
+package com.MeloHub.MeloHub;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MeloHubApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MeloHubApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/MeloHub/MeloHub/MeloHubApplicationTests.java
@@ -0,0 +1,13 @@
+package com.MeloHub.MeloHub;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MeloHubApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}

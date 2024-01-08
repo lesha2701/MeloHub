029e501bc01e3a2bc986618418d7e47c8aa09252
Алексей Артемович <81881832+lesha2701@users.noreply.github.com>
2024-01-08 15:35:31 +0300

    add vue and song component

---
+++ front/meloHub/src/views/HomeView.vue
@@ -0,0 +1,15 @@
+<script>
+import song from '@/components/song.vue';
+
+export default {
+  components: {
+    song
+  }
+}
+</script>
+
+<template>
+  <main>
+    <song></song>
+  </main>
+</template>
